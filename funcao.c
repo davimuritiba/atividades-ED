5acdb23e627f04f41aa353535e4c533815da035b
davi muritiba <145371341+davimuritiba@users.noreply.github.com>
2024-11-04 11:48:04 -0300

    Update funcao.c

--- funcao.c
+++ funcao.c
@@ -2,7 +2,6 @@ void distribuirLivrosNasEstantes(Biblioteca* biblioteca, Livro* listaLivros) {
     Estante* estanteAtual = biblioteca->estantes;
     int prateleiraAtual = 0;
 
-
     if (estanteAtual == NULL) {
         adicionarEstante(biblioteca);  // Garante que exista ao menos uma estante
         estanteAtual = biblioteca->estantes;
@@ -13,24 +12,26 @@ void distribuirLivrosNasEstantes(Biblioteca* biblioteca, Livro* listaLivros) {
     int volumeMaximo = 100000;
     int volumeAtual = 0;
     Livro* livroAtual = listaLivros;
+    
     while (livroAtual != NULL) {
         Prateleira* prateleira = &estanteAtual->prateleiras[prateleiraAtual];
         int areaLivro = livroAtual->largura * livroAtual->profundidade;
 
-        if (areaLivro <= capacidadeRestante && volumeAtual <= volumeMaximo) {
-            estanteAtual->prateleiras[prateleiraAtual].livros = livroAtual;
+        // Verifica se o livro cabe na prateleira pelo espaço e pelo volume
+        if (areaLivro <= capacidadeRestante && (volumeAtual + livroAtual->volume) <= volumeMaximo) {
+            prateleira->livros = livroAtual;
             capacidadeRestante -= areaLivro;
             volumeAtual += livroAtual->volume;
             livroAtual = livroAtual->next;
-        } 
-        else {
-            prateleiraAtual++; //vai para a proxima prateleira
+        } else {
+            // Vai para a próxima prateleira
+            prateleiraAtual++;
             if (prateleiraAtual == 6) {
                 adicionarEstante(biblioteca);
                 estanteAtual = estanteAtual->next;
                 prateleiraAtual = 0;
             }
-            areaPrateleira = 32 * 96;
+            // Redefine a capacidade e o volume para a nova prateleira
             capacidadeRestante = areaPrateleira;
             volumeAtual = 0;
         }
